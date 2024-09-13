60ec974c8265ea588fda9a1cf3d155b041285a98
quincshe <quincshe@student.21-school.ru>
2024-09-13 12:52:14 +0700

    prefix increment replaced with postfix, method fixed delete

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -4,10 +4,10 @@
 public class ArrayStorage {
 
     Resume[] storage = new Resume[10000];
-    int size = 0;
+    int size;
 
     void clear() {
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size; i++) {
             storage[i] = null;
         }
         size = 0;
@@ -16,12 +16,12 @@ public class ArrayStorage {
     void save(Resume r) {
         if (size < storage.length) {
             storage[size] = r;
-            ++size;
+            size++;
         }
     }
 
     Resume get(String uuid) {
-        for (int i = 0; i < size; ++i) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
                 return storage[i];
             }
@@ -30,19 +30,12 @@ public class ArrayStorage {
     }
 
     void delete(String uuid) {
-        int i = 0;
-        int removedPosition = size;
-        for (; i < size; ++i) {
+        for (int i = 0; i < size; i++) {
             if (storage[i].uuid.equals(uuid)) {
-                removedPosition = i;
+                storage[i] = storage[size - 1];
+                storage[size - 1] = null;
                 size--;
-            }
-        }
-        for (int k = removedPosition; k < size; ++k) {
-            if (k < storage.length - 1) {
-                storage[k] = storage[k + 1];
-            } else {
-                storage[k] = null;
+                break;
             }
         }
     }
