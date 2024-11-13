b0691fadbe7d247c1df5bfdee9dc82c6a990fb16
quincshe <quincshe@student.21-school.ru>
2024-11-13 17:24:44 +0700

    refactor Resume

--- src/com/urise/webapp/model/Resume.java
+++ src/com/urise/webapp/model/Resume.java
@@ -46,8 +46,16 @@ public class Resume implements Comparable<Resume> {
         return contacts;
     }
 
+    public String getContact(ContactType type){
+        return contacts.get(type);
+    }
+
     public Map<SectionType, Section> getSections() {
         return sections;
+    }
+
+    public Section getSection(SectionType type){
+        return sections.get(type);
     }
 
     public void setFullName(String fullName) {
