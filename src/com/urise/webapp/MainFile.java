2ea46bd6bd311fd9a8059d499055b7c49125c5a2
quincshe <quincshe@student.21-school.ru>
2024-11-21 15:02:28 +0700

    print catalogs with tabs

--- src/com/urise/webapp/MainFile.java
+++ src/com/urise/webapp/MainFile.java
@@ -5,39 +5,44 @@ import java.io.FileInputStream;
 import java.io.IOException;
 
 public class MainFile {
+
     public static void main(String[] args) {
         String filePath = "./.gitignore";
 
-        File file = new File(filePath);
-        try {
-            System.out.println(file.getCanonicalPath());
-        } catch (IOException e) {
-            throw new RuntimeException("Error", e);
-        }
+//        File file = new File(filePath);
+//        try {
+//            System.out.println(file.getCanonicalPath());
+//        } catch (IOException e) {
+//            throw new RuntimeException("Error", e);
+//        }
 
-        File dir = new File("./src/com/urise/webapp");
-        System.out.println(dir.isDirectory());
-        readDirectory(dir);
+        File dir = new File("/home/quincshe/JAVAOPS/basejava/src");
+//        System.out.println(dir.isDirectory());
+        readDirectory(dir, 0);
 
-        try (FileInputStream fis = new FileInputStream(filePath)) {
-            System.out.println(fis.read());
-        } catch (IOException e) {
-            throw new RuntimeException(e);
-        }
+//        try (FileInputStream fis = new FileInputStream(filePath)) {
+//            System.out.println(fis.read());
+//        } catch (IOException e) {
+//            throw new RuntimeException(e);
+//        }
     }
 
-    private static void readDirectory(File dir){
+    private static void readDirectory(File dir, int count) {
         File[] files = dir.listFiles();
         if (files != null) {
             for (File file : files) {
-                if (file.isDirectory()){
-                    System.out.println(file.getName() +"->");
-                    readDirectory(file);
-                } else {
-                    System.out.println(file.getName());
+                printTabs(count);
+                System.out.println(file.getName());
+                if (file.isDirectory()) {
+                    readDirectory(file, count + 1);
                 }
             }
         }
-        System.out.println();
+    }
+
+    private static void printTabs(int c) {
+        for (int i = 0; i < c; i++) {
+            System.out.print("\t");
+        }
     }
 }
