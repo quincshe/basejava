5b33c071bc2ed0f0ceb566010f970dfb2c15b77a
quincshe <quincshe@student.21-school.ru>
2024-12-10 16:27:06 +0700

    Data strategy start(name, contacts)

--- test/com/urise/webapp/storage/DataPathStorageTest.java
+++ test/com/urise/webapp/storage/DataPathStorageTest.java
@@ -1,7 +1,6 @@
 package com.urise.webapp.storage;
 
 import com.urise.webapp.storage.strategies.DataStrategy;
-import com.urise.webapp.storage.strategies.ObjectStrategy;
 
 public class DataPathStorageTest extends AbstractStorageTest {
 
